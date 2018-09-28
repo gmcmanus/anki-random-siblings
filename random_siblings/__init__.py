2baee5c66de5cc442379e12e1bf97b1a1dff68cf
gmcmanus <gmcmanus@users.noreply.github.com>
2018-09-28 13:28:08 +0000

    Initial commit

---
+++ random_siblings/__init__.py
@@ -0,0 +1,51 @@
+import random
+
+from anki.collection import _Collection
+from anki.sched import Scheduler
+from anki.consts import NEW_CARDS_DUE
+from anki.utils import ids2str, intTime
+
+
+def _dueForDid(self, did, due):
+    conf = self.decks.confForDid(did)
+    # in order due?
+    if conf['new']['order'] == NEW_CARDS_DUE:
+        return due
+    else:
+        # random mode
+        return random.randrange(1, max(due, 1000))
+
+_Collection._dueForDid = _dueForDid
+
+
+def sortCards(self, cids, start=1, step=1, shuffle=False, shift=False):
+    scids = ids2str(cids)
+    now = intTime()
+    # determine cid ordering
+    due = {}
+    if shuffle:
+        cids = list(cids)
+        random.shuffle(cids)
+    for c, cid in enumerate(cids):
+        due[cid] = start+c*step
+    high = start+c*step
+    # shift?
+    if shift:
+        low = self.col.db.scalar(
+            "select min(due) from cards where due >= ? and type = 0 "
+            "and id not in %s" % scids,
+            start)
+        if low is not None:
+            shiftby = high - low + 1
+            self.col.db.execute("""
+update cards set mod=?, usn=?, due=due+? where id not in %s
+and due >= ? and queue = 0""" % scids, now, self.col.usn(), shiftby, low)
+    # reorder cards
+    d = []
+    for cid, in self.col.db.execute(
+        "select id from cards where type = 0 and id in "+scids):
+        d.append(dict(now=now, due=due[cid], usn=self.col.usn(), cid=cid))
+    self.col.db.executemany(
+        "update cards set due=:due,mod=:now,usn=:usn where id = :cid", d)
+
+Scheduler.sortCards = sortCards
